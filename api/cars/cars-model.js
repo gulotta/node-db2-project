46ff23943a3eec19099b01e505590dc63b40fa89
Michael Gulotta <gulottam@gmail.com>
2023-10-04 16:57:27 -0400

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,31 +1,11 @@
-const knex = require('knex')
-
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './data/dealer.db3'
-  },
-  useNullAsDefault: true
-})
-
-function getAll = () => {
+const getAll = () => {
   // DO YOUR MAGIC
-  return db('cars')
 }
 
-function getById = (id) => {
+const getById = () => {
   // DO YOUR MAGIC
-  return db('cars').where('id', id).first()
 }
 
-async function create(car) {
+const create = () => {
   // DO YOUR MAGIC
-  const [id] = await db('cars').insert(car);
-  return getById(id)
-}
-
-module.exports = {
-  getAll,
-  getById,
-  create
 }
