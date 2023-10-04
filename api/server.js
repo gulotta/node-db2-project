c9f012c65703f38404d0c043a0dff6082ba6a532
Michael Gulotta <gulottam@gmail.com>
2023-10-04 16:50:21 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -4,8 +4,17 @@ const server = express()
 
 server.use(express.json())
 
+server.use('*', (req, res, next) => {
+    next({status: 404, message: 'not found'})
+})
+
 server.use('/api/cars', carsRouter)
 
 // DO YOUR MAGIC
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 
 module.exports = server
