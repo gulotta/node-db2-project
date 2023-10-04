5e1407ed6de47f7f87a405883446c4e61ca8dcfe
Michael Gulotta <gulottam@gmail.com>
2023-10-04 16:36:42 -0400

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+const Cars = require('./cars-model')
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    Cars.getAll()
+    .then(cars => {
+        res.json(cars)
+    })
+    .catch(err => {
+        res.status(500).json({ 
+            message: `Failed to retrieve cars: ${err.message}`});
+    })
+})
+
+module.exports = router
